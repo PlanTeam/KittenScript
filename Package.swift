2df8aacf519c63de5c28c15abf6c509c1f290f85
Robbert Brandsma <i@robbertbrandsma.nl>
2016-08-24 13:27:35 +0200

    Add CommonKitten

--- Package.swift
+++ Package.swift
@@ -2,5 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "KittenScript",
+    dependencies: [
+        .Package(url: "https://github.com/OpenKitten/CommonKitten.git", majorVersion: 0)
+    ],
     exclude: ["Scripts"]
 )
