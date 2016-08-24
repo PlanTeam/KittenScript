4c913b1fe49c68a68637439df0492b09c67bed4c
Robbert Brandsma <i@robbertbrandsma.nl>
2016-08-24 14:13:34 +0200

    Compiler basics

--- Sources/KittenScript/main.swift
+++ Sources/KittenScript/main.swift
@@ -1,12 +1,16 @@
 import KittenRunner
 import KittenCompiler
 
-print("Meow.")
-var script: [UInt8] = [
-    14,0,0,0, // undefined length now
-    0x04 /*plain expression*/, 0x02 /*dynamic function call*/, 0x68 /*h*/, 0x65 /*e*/, 0x6c /*l*/, 0x6c /*l*/, 0x6f /*o*/, 0x00, 0x00 /*empty parameter list*/,
-    0x00
-]
+//var uncompiled = "hello()\nwhatsup(    )    hello() hello() whatsup() hello()"
+let uncompiled = "hello()hello()whatsup()hello()hello()hello()hello()"
+
+var script = try! compile(uncompiled)
+
+//var script: [UInt8] = [
+//    14,0,0,0, // undefined length now
+//    0x04 /*plain expression*/, 0x02 /*dynamic function call*/, 0x68 /*h*/, 0x65 /*e*/, 0x6c /*l*/, 0x6c /*l*/, 0x6f /*o*/, 0x00, 0x00 /*empty parameter list*/,
+//    0x00
+//]
 
 let runnableCode = Code()
 runnableCode.code = script
@@ -14,4 +18,10 @@ runnableCode.functions["hello"] = { parameters in
     print("hello")
     return .null
 }
+runnableCode.functions["whatsup"] = { parameters in
+    print("OMG IT ASKED ME WHAT'S UP THIS IS SO EXCIIIIITING")
+    return .null
+    
+}
+
 try! runnableCode.run(inContext: [:])
