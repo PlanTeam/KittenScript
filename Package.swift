f36a7b46e92841e11fa8a287942d49e7e20e1eac
Joannis Orlandos <j.orlandos@jakajima.eu>
2016-08-24 13:30:19 +0200

    Targets

--- Package.swift
+++ Package.swift
@@ -2,6 +2,10 @@ import PackageDescription
 
 let package = Package(
     name: "KittenScript",
+    targets: [
+        Target(name: "KittenRunner"),
+        Target(name: "KittenCompiler")
+    ],
     dependencies: [
         .Package(url: "https://github.com/OpenKitten/CommonKitten.git", majorVersion: 0)
     ],
